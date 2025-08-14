69cb448dfba45233dada91af749eac7ba85b93b2
Pablo Ordoñez <3044029070114@ingenieria.usac.edu.gt>
2025-08-13 18:28:51 -0600

    Prueba texto

--- Practica1/src/main/java/com/mycompany/practica1/Practica1.java
+++ Practica1/src/main/java/com/mycompany/practica1/Practica1.java
@@ -11,6 +11,6 @@ package com.mycompany.practica1;
 public class Practica1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello PRACTICA 1!");
     }
 }
