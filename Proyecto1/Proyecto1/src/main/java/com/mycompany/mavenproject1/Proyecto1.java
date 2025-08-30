515a0ec693b26132f27bc71a5b654f89ca3cb0d0
Pablo Ordoñez <3044029070114@ingenieria.usac.edu.gt>
2025-08-29 22:37:15 -0600

    Creacion del menu de opciones

--- Proyecto1/Proyecto1/src/main/java/com/mycompany/mavenproject1/Proyecto1.java
+++ Proyecto1/Proyecto1/src/main/java/com/mycompany/mavenproject1/Proyecto1.java
@@ -4,13 +4,34 @@
 
 package com.mycompany.mavenproject1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pablo
  */
 public class Proyecto1 {
-
+    static Scanner entrada = new Scanner(System.in);
+    
+    static int opcion = 0;
     public static void main(String[] args) {
-        System.out.println("Proyecto 1!");
+        while(opcion!=8){
+            System.out.println("-------- MENÚ PRINCIPAL --------");
+            System.out.println("Seleccione una opcion: ");
+            System.out.println("1. Agregar Producto");
+            System.out.println("2. Buscar Producto");
+            System.out.println("3. Eliminar Producto");
+            System.out.println("4. Regitrar Venta");
+            System.out.println("5. Generar Reportes");
+            System.out.println("6. Ver datos del estudiante");
+            System.out.println("7. Bitacora");
+            System.out.println("9. Salir");
+            opcion = entrada.nextInt();
+            
+        }
+        
+        
+        
+        
     }
 }
