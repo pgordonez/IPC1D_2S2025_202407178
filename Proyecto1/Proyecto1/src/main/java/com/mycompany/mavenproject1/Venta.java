12fef70cc245d3b78b520e110d73aa6c8b26f562
Pablo Ordoñez <3044029070114@ingenieria.usac.edu.gt>
2025-09-10 18:18:32 -0600

    Clase venta

---
+++ Proyecto1/Proyecto1/src/main/java/com/mycompany/mavenproject1/Venta.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+import java.text.SimpleDateFormat;  //Para formatear fechas en texto legible
+import java.util.Date;      //Sirve para obtener la fecha y hora actual del sistema
+
+/**
+ *
+ * @author pablo
+ */
+public class Venta {
+    //Atributos
+    String codigoProducto; //Identificador del producto
+    int cantidadVendida;    //Unidades vendidas
+    String fechaHora;       //Momento en que se realizó la venta
+    double total;           //monto total de la venta
+    
+    //Constructor
+    public Venta(String codigoProducto, int cantidadVendida, String fechaHora, double total){
+        this.codigoProducto = codigoProducto;
+        this.cantidadVendida = cantidadVendida;
+        this.total = total;
+        
+        //Generacion de fecha/hora
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");       //Da formato al texto
+        this.fechaHora = formatter.format(new Date());      //Obtiene la fecha actual del sistema
+    }
+    
+    //Metodo Para mostrar informacion
+    public void mostrarInfo(){
+        System.out.println("Producto: "+ codigoProducto);
+        System.out.println("Cantidad: "+ cantidadVendida);
+        System.out.println("Total: "+   total);
+        System.out.println("Fecha: "+ fechaHora);
+        System.out.println("----------------------");
+    }
+}
